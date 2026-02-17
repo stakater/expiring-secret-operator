8e00309bf3950dd701edea646d3171018eebabf7
Jonas Marklen @ Stakater <jonas.marklen@stakater.com>
2026-02-17 17:33:40 +0100

    Fixes mistakes from the initial (#3)
    * rewrite operator scaffolding, metrics, controller coverage and bump versions
    * preallocating variable
    * output to file
    * disable TTY-input

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 VERSION ?= 0.0.1
-OPERATOR_NAME ?= expiring-secrets
+OPERATOR_NAME ?= expiring-secret-operator
+#OPERATOR_NAME ?= expiring-secrets
 PROJECT_NAME ?= expiring-secrets.stakater.com
-TEST_CLUSTER_NAME ?= kind
-DOCKER_REPO_NAME ?= expiring-secret-operator
+#vimTEST_CLUSTER_NAME ?= kind
+#DOCKER_REPO_NAME ?= expiring-secret-operator
 #SUPPRESS_OUTPUT ?= false
 
 TEST_ARGS ?= -v -test.v -ginkgo.v
@@ -12,5 +13,23 @@ TEST_ARGS ?= -v -test.v -ginkgo.v
 
 .PHONY: pre-commit
 pre-commit: precheck check_license fmt vet lint
+
+.PHONY: e2e
+e2e: undeploy gh-action test-e2e
+
+.PHONY: gh-action
+gh-action:
+#	@{ \
+#		$(MAKE) manifests build docker-build; \
+#		$(MAKE) bundle bundle-build; \
+#		$(MAKE) docker-build bundle-build ; \
+#		$(MAKE) catalog-render catalog-build; \
+#		$(MAKE) deploy; \
+#	};
+	@{ \
+		$(MAKE) manifests build docker-build; \
+		$(MAKE) bundle bundle-build; \
+		$(MAKE) deploy; \
+	}
 
 include Makefile.common
--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -17,16 +17,64 @@ limitations under the License.
 package e2e
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
+	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	expiringsecretv1alpha1 "github.com/stakater/expiring-secret-operator/api/v1alpha1"
+)
+
+var (
+	k8sClient client.Client
+	ctx       context.Context
+	log       logr.Logger
 )
 
 // Run e2e tests using the Ginkgo runner.
 func TestE2E(t *testing.T) {
 	RegisterFailHandler(Fail)
-	_, _ = fmt.Fprintf(GinkgoWriter, "Starting expiring-secrets suite\n")
+	_, _ = fmt.Fprintf(GinkgoWriter, "Starting expiring-secret-operator suite\n")
 	RunSpecs(t, "e2e suite")
 }
+
+var _ = BeforeSuite(func() {
+	By("setting up the test-e2e suite")
+
+	By("  - logger")
+	log = zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true))
+	Expect(log).NotTo(BeNil())
+	logf.SetLogger(log)
+	log.Info("Logger initialized for e2e tests")
+
+	By("  - context")
+	ctx = context.Background()
+
+	By("  - logger -> context")
+	logf.IntoContext(ctx, log)
+
+	By("  - kubernetes client")
+	cfg, err := config.GetConfig()
+	Expect(err).NotTo(HaveOccurred())
+
+	By("  - schemas")
+	err = expiringsecretv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	By("  - k8sClient")
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+})
+
+var _ = AfterSuite(func() {
+	By("tearing down the test-e2e suite")
+})
